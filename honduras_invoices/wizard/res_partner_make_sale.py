29a353494554b83675a5b600d61a3004b00e778b
LuisMalave2001 <LuisAngelMalaveMora@gmail.com>
2020-04-03 08:44:14 +0200

    asd

--- honduras_invoices/wizard/res_partner_make_sale.py
+++ honduras_invoices/wizard/res_partner_make_sale.py
@@ -6,8 +6,7 @@ class ResPartnerMakeSale(models.TransientModel):
     _name = "res.partner.make.sale"
     _description = "Make a sale for a partner"
 
-    order_new = fields.Char("NWEWW")
-    order_line_ids = fields.Many2many("sale.order.line", string="Order Lines")
+    order_line_ids = fields.Many2many("sale.order.line", string="Order Lines", ondelete="cascade")
 
     def make_sale(self):
         partner_ids = self.env["res.partner"].browse(self.env.context.get("active_ids", []))
