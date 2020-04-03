77d8173211967a7c3aaeabf52ce6517d415a7c2d
LuisMalave2001 <LuisAngelMalaveMora@gmail.com>
2020-04-03 08:40:26 +0200

    asd

--- hr_extends/__manifest__.py
+++ hr_extends/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Human resources',
-    'version': '0.1',
+    'version': '0.2.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', "hr", "hr_contract"],
